04a45ff063311467a228a59b49d907f424056378
Shubham <shubhamvishu171@gmail.com>
2018-03-04 11:36:52 +0530

    Initial commit

---
+++ person.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class admin{
+    char encryp[20];
+	char password[20];
+	void pass();
+ public:void access();
+        void new_record();		
+};
+class person{
+	char hair_color[20];
+	char hair_type[20];
+	int specs;
+	int height_min;
+	int height_max;
+	int age_min;
+	int age_max;
+	chae gender;
+	
+};
+int main()
+{
+	
+}
